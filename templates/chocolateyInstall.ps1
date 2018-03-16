6eb0811305b82771b01d09e4ee84d515335576a2
David Beck <techiscool@gmail.com>
2018-03-15 17:56:25 -0700

    [Add] Metrics Firewall Rules

--- templates/chocolateyInstall.ps1
+++ templates/chocolateyInstall.ps1
@@ -17,21 +17,21 @@ $choco_params = @{
 Install-ChocolateyZipPackage @choco_params
 
 
-# New-NetFirewallRule -Name "Gobetween-Metrics-HTTP-In-TCP" `
-# -DisplayName "Gobetween (HTTP-In)" `
-# -Description "Inbound rule for Gobetween Metrics. [TCP 9284]" `
-# -Protocol TCP `
-# -LocalPort "9284" `
-# -Action Allow `
-# -Profile Domain,Private
+New-NetFirewallRule -Name "Gobetween-Metrics-HTTP-In-TCP" `
+-DisplayName "Gobetween (HTTP-In)" `
+-Description "Inbound rule for Gobetween Metrics. [TCP 9284]" `
+-Protocol TCP `
+-LocalPort "9284" `
+-Action Allow `
+-Profile Domain,Private
 
-# New-NetFirewallRule -Name "Gobetween-Metrics-HTTP-In-TCP-PUBLIC" `
-# -DisplayName "Gobetween (HTTP-In)" `
-# -Description "Inbound rule for Gobetween Metrics. [TCP 9284]" `
-# -Protocol TCP `
-# -LocalPort "9284" `
-# -Action Allow `
-# -Profile Public
+New-NetFirewallRule -Name "Gobetween-Metrics-HTTP-In-TCP-PUBLIC" `
+-DisplayName "Gobetween (HTTP-In)" `
+-Description "Inbound rule for Gobetween Metrics. [TCP 9284]" `
+-Protocol TCP `
+-LocalPort "9284" `
+-Action Allow `
+-Profile Public
 
 &nssm install "$package" "C:\ProgramData\${package}\${package}.exe" "$($env:chocolateyPackageParameters)"
 &nssm set "$package" Start SERVICE_AUTO_START
--- templates/chocolateyUninstall.ps1
+++ templates/chocolateyUninstall.ps1
@@ -1,3 +1,6 @@
 ﻿$package = '##PACKAGENAME##'
 
+Remove-NetFirewallRule -Name "Gobetween-Metrics-HTTP-In-TCP"
+Remove-NetFirewallRule -Name "Gobetween-Metrics-HTTP-In-TCP-PUBLIC"
+
 Remove-Item "C:\ProgramData\${package}" -Recurse -Force
